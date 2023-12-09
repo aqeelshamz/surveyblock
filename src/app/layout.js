c2e0b4d518faf238f58861f70d2412d8b919fd59
Abdul Majid <majid616365@gmail.com>
2023-12-09 15:22:50 +0530

    f: update anonadhaar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,44 @@
 "use client";
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { getDefaultWallets, RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  ConnectButton,
+} from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { polygonMumbai } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import "@rainbow-me/rainbowkit/styles.css";
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const app_id = process.env.NEXT_PUBLIC_APP_ID;
+
   const scroll = {
     id: 534351,
-    name: 'Scroll Sepolia Testnet',
-    network: 'Sepolia',
-    iconUrl: 'https://pbs.twimg.com/profile_images/1696531511519150080/Fq5O0LeN_400x400.jpg',
-    iconBackground: '#fff',
+    name: "Scroll Sepolia Testnet",
+    network: "Sepolia",
+    iconUrl:
+      "https://pbs.twimg.com/profile_images/1696531511519150080/Fq5O0LeN_400x400.jpg",
+    iconBackground: "#fff",
     nativeCurrency: {
       decimals: 18,
-      name: 'Scroll ETH',
-      symbol: 'ETH',
+      name: "Scroll ETH",
+      symbol: "ETH",
     },
     rpcUrls: {
-      public: { http: ['https://sepolia-rpc.scroll.io'] },
-      default: { http: ['https://sepolia-rpc.scroll.io'] },
+      public: { http: ["https://sepolia-rpc.scroll.io"] },
+      default: { http: ["https://sepolia-rpc.scroll.io"] },
     },
     testnet: true,
   };
   const { chains, publicClient } = configureChains(
     [scroll, polygonMumbai],
-    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()],
+    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
   );
 
   var wagmiConfig;
@@ -57,9 +65,13 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         <WagmiConfig config={wagmiConfig} className="sticky top-0 z-50">
-          <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+          <RainbowKitProvider chains={chains}>
+            <AnonAadhaarProvider _appId={app_id}>
+              {children}
+            </AnonAadhaarProvider>
+          </RainbowKitProvider>
         </WagmiConfig>
       </body>
     </html>
-  )
+  );
 }
