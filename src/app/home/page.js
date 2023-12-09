e81efa77daf80ccec2c4fecd1cf78d8ee3bc4fb2
Aqeel <aqeelten@gmail.com>
2023-12-09 17:35:59 +0530

    survey creation

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -4,7 +4,8 @@ import Navbar from "../../components/Navbar";
 import WeaveDB from "weavedb-sdk";
 import { useAccount } from "wagmi";
 import { CiCirclePlus } from "react-icons/ci";
-import { FiPlusCircle } from "react-icons/fi";
+import { FiBox, FiPlusCircle } from "react-icons/fi";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
   const [responses, setResponses] = useState([]);
@@ -16,6 +17,9 @@ export default function Home() {
   const [creatingSurvey, setCreatingSurvey] = useState(false);
 
   const [surveys, setSurveys] = useState([]);
+
+  const [newSurveyName, setNewSurveyName] = useState("");
+  const [newSurveyDescription, setNewSurveyDescription] = useState("");
 
   const initDB = async () => {
     setLoadingSurveys(true);
@@ -36,6 +40,36 @@ export default function Home() {
     }
     setDB(db);
     setLoadingSurveys(false);
+  };
+
+  const createSurvey = async () => {
+    setCreatingSurvey(true);
+
+    const surveyId = uuidv4();
+    const surveyData = {
+      id: surveyId,
+      author: db.signer(),
+      title: newSurveyName,
+      description: newSurveyDescription,
+      fields: [
+        { id: "a28b29", title: "Name", type: "text", required: true },
+        { id: "z2cx29", title: "Feedback", type: "longtext", required: true },
+      ],
+      responses: 0,
+    };
+
+    const tx = await db.add(surveyData, "surveys");
+    console.log(tx);
+
+    console.log("Saved to DB");
+
+    setCreatingSurvey(false);
+
+    document.getElementById("newsurvey_modal").close();
+
+    setTimeout(() => {
+      window.location.href = "/editor/" + surveyId;
+    }, 5000);
   };
 
   useEffect(() => {
@@ -71,13 +105,13 @@ export default function Home() {
                 </div>
               </div>
               {
-                surveys?.map((form, index) => {
-                  return <Section key={index} translate="translateY(10px)" duration={((index * 0.075) + 0.5).toString() + "s"}><div onClick={() => (window.location.href = "/editor/" + form?.data?.id)} className="hover:shadow-2xl duration-100 cursor-pointer border-2 flex flex-col h-full w-full mb-10 mr-10 rounded-3xl shadow-lg overflow-hidden">
-                    <div style={{ background: `linear-gradient(45deg, ${bgColors[form?.data?.title.toString().toLowerCase()[0]][0]}, ${bgColors[form?.data?.title.toString().toLowerCase()[0]][1]})` }} className={"flex items-center justify-center w-full h-full"}>
-                      <FiFileText style={{ color: bgColors[form?.data?.title.toString().toLowerCase()[0]][1] }} className={"h-40 w-40 mb-2"} />
+                surveys?.map((survey, index) => {
+                  return <Section key={index} translate="translateY(10px)" duration={((index * 0.075) + 0.5).toString() + "s"}><div onClick={() => (window.location.href = "/editor/" + survey?.data?.id)} className="hover:shadow-2xl duration-100 cursor-pointer border-2 flex flex-col h-full w-full mb-10 mr-10 rounded-3xl shadow-lg overflow-hidden">
+                    <div style={{ background: `linear-gradient(45deg, ${bgColors[survey?.data?.title.toString().toLowerCase()[0]][0]}, ${bgColors[survey?.data?.title.toString().toLowerCase()[0]][1]})` }} className={"flex items-center justify-center w-full h-full"}>
+                      <FiFileText style={{ color: bgColors[survey?.data?.title.toString().toLowerCase()[0]][1] }} className={"h-40 w-40 mb-2"} />
                     </div>
                     <div className="p-5 h-auto">
-                      <p className="font-semibold text-lg">{form?.data?.title}</p>
+                      <p className="font-semibold text-lg">{survey?.data?.title}</p>
                       <p className="text-gray-500">No responses</p>
                     </div>
                   </div></Section>
@@ -88,24 +122,26 @@ export default function Home() {
         )}
       </main>
       <dialog id="newsurvey_modal" className="modal">
-				<div className="modal-box max-w-xl">
-					<h3 className="flex items-center font-bold text-2xl"><FiPlusCircle className="mr-2" /> Create survey</h3>
-					{creatingSurvey ? (
-						"Creating survey..."
-					) : (
-						<div className="flex mt-6 max-w-fulloverflow-hidden">
-							
-						</div>
-					)}
-					<div className="modal-action">
-						<form method="dialog">
-							<button className="btn">Close</button>
-						</form>
-					</div>
-				</div>
-			</dialog>
+        <div className="modal-box max-w-xl">
+          <h3 className="flex items-center font-bold text-2xl"><FiBox className="mr-2" /> Create survey</h3>
+          {creatingSurvey ? (
+            "Creating survey..."
+          ) : (
+            <div className="flex flex-col mt-6 max-w-fulloverflow-hidden">
+              <p className="mb-2">Survey Name</p>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full mb-5" value={newSurveyName} onChange={(x) => setNewSurveyName(x.target.value)} />
+              <p className="mb-2">Description</p>
+              <textarea className="textarea textarea-bordered mb-5" placeholder="Description" value={newSurveyDescription} onChange={(x) => setNewSurveyDescription(x.target.value)}></textarea>
+            </div>
+          )}
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+              <button className="btn btn-primary ml-2" onClick={createSurvey}>Create Survey</button>
+          </div>
+        </div>
+      </dialog>
     </div>
   )
 }
-
-export const dynamic = 'force-dynamic'
