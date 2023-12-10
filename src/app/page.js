1d990422ee35f5d1f122b5ed93caee9a38c1271e
Aqeel <aqeelten@gmail.com>
2023-12-10 07:14:28 +0530

    ui update

--- src/app/page.js
+++ src/app/page.js
@@ -15,10 +15,10 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="w-full h-[100vh] flex items-center justify-center bg-gradient-to-tr from-[#0b0e3b] to-[#1c134c]">
+    <main className="w-full h-[100vh] flex items-center justify-center bg-gradient-to-tr from-[#00ffa25e] to-[#266eff63]">
       <div className="flex flex-col items-center">
-        <h1 className="font-bold flex items-center text-8xl text-white"><FiBox className="mr-2"/> SurveyBlock</h1>
-        <h1 className="text-4xl mt-6 text-white">Decentralised Survey Creator</h1>
+        <h1 className="font-bold flex items-center text-8xl"><FiBox className="mr-2"/> SurveyBlock</h1>
+        <h1 className="text-4xl mt-6">Decentralised Survey Forms Creator</h1>
         {domLoaded ? (
           <div className="mt-10">
             {address ? (
